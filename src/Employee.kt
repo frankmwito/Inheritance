1669dd0796a45894e8223eca132ab949dd237b1d
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-08 16:46:13 +0300

    Employee Hierarchy

--- src/Employee.kt
+++ src/Employee.kt
@@ -1,1 +1,50 @@
+import java.util.Scanner
+
 // Employee Hierarchy
+
+open class Employee(
+    val salary: Double,
+    val name:String
+){
+    open fun calculateBonus(){
+        println("The salary is $salary")
+    }
+}
+
+class Manager(name: String, salary: Double): Employee(salary, name){
+    override fun calculateBonus() {
+
+        val bonus = salary + (0.2* salary)
+        println("The bonus salary for $name is $bonus")
+    }
+}
+class Engineer(name: String,salary: Double): Employee(salary, name){
+
+    override fun calculateBonus() {
+        val bonus = salary + (0.275* salary)
+        println("The bonus salary for $name is $bonus")
+    }
+    }
+fun main() {
+    val reader = Scanner(System.`in`)
+    println("Enter your choice for bonus, 1 for manager and 2 for engineer")
+   val choice = reader.nextInt()
+
+    when(choice){
+        1 -> {
+            println("Enter the name of manager")
+            println("Enter the salary of manager")
+            val manager = Manager(reader.next(), reader.nextDouble())
+            manager.calculateBonus()
+        }
+        2-> {
+            println("Enter the name of engineer")
+            println("Enter the salary of engineer")
+            val engineer = Engineer(reader.next(), reader.nextDouble())
+            engineer.calculateBonus()
+        }
+        else-> println("Invalid choice")
+    }
+
+}
+
